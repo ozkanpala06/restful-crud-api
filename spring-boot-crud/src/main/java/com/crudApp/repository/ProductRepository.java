150dfb219c57179beb72efba03f6826dbfff9d62
Özkan Pala <ozkanpala729@gmail.com>
2021-06-20 20:30:39 +0300

    restful-crud-api

---
+++ spring-boot-crud/src/main/java/com/crudApp/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.crudApp.repository;
+
+import com.crudApp.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
